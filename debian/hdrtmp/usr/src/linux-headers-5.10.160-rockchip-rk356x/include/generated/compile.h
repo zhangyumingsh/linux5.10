85c26a885741828ca4d290be496e9ec0739668a1
zhangyumingsh <492964829@qq.com>
2024-02-02 17:33:45 +0800

    second

---
+++ debian/hdrtmp/usr/src/linux-headers-5.10.160-rockchip-rk356x/include/generated/compile.h
@@ -0,0 +1,7 @@
+/* This file is auto generated, version 1.0.4 */
+/* SMP */
+#define UTS_MACHINE "aarch64"
+#define UTS_VERSION "#1.0.4 SMP Thu Feb 1 16:42:23 CST 2024"
+#define LINUX_COMPILE_BY "root"
+#define LINUX_COMPILE_HOST "falcon"
+#define LINUX_COMPILER "aarch64-none-linux-gnu-gcc (GNU Toolchain for the A-profile Architecture 9.2-2019.12 (arm-9.10)) 9.2.1 20191025, GNU ld (GNU Toolchain for the A-profile Architecture 9.2-2019.12 (arm-9.10)) 2.33.1.20191209"
